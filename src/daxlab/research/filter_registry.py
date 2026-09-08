f6b67a8ddbfa4a02fe80a72d3899c0af9301250e
hennebergtoni-lgtm <henneberg.toni@gmail.com>
2026-09-08 07:12:18 +0200

    filter_registry.py aktualisieren

--- src/daxlab/research/filter_registry.py
+++ src/daxlab/research/filter_registry.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum
-from typing import Iterable
+from collections.abc import Iterable
 
 
 class EvidenceStatus(str, Enum):
