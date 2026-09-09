301a27bdab65ff02f8ca1f27eb1f9deae1e27fd4
hennebergtoni-lgtm <henneberg.toni@gmail.com>
2026-09-09 11:15:46 +0200

    Pin soak checkpoint schema version

---
+++ tests/test_soak_checkpoint_schema.py
@@ -0,0 +1,5 @@
+from daxlab.runtime.shadow_soak import initial_soak_checkpoint
+
+
+def test_soak_checkpoint_schema_version_is_explicit() -> None:
+    assert initial_soak_checkpoint().schema_version == "DAXLAB_SHADOW_SOAK_CHECKPOINT_V1"
