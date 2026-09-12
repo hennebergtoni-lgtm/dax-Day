1c75bcf783572e80d79b865492791a486385c56d
hennebergtoni-lgtm <henneberg.toni@gmail.com>
2026-09-12 18:12:11 +0200

    Step 2147: export atomic file state store adapter

--- src/daxlab/adapters/__init__.py
+++ src/daxlab/adapters/__init__.py
@@ -1,5 +1,6 @@
 """External-system adapters for the DAX-BOT NextGen product core."""
 
+from daxlab.adapters.file_state_store import AtomicFileStateStore
 from daxlab.adapters.mt5_market_data import Mt5ClosedM5CandleSource
 
-__all__ = ["Mt5ClosedM5CandleSource"]
+__all__ = ["AtomicFileStateStore", "Mt5ClosedM5CandleSource"]
