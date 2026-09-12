410ed8349dae22112862b56ae21829ab24782c34
hennebergtoni-lgtm <henneberg.toni@gmail.com>
2026-09-12 18:36:06 +0200

    Step 2151: export CAND-001 state codec

--- src/daxlab/strategies/cand001/__init__.py
+++ src/daxlab/strategies/cand001/__init__.py
@@ -6,8 +6,16 @@ from daxlab.strategies.cand001.adapter import (
     adapt_cand001_runtime_candle,
     map_cand001_strategy_decision,
 )
+from daxlab.strategies.cand001.state_codec import (
+    CAND001_STATE_CODEC_ID,
+    CAND001_STATE_SCHEMA,
+    Cand001PipelineStateCodec,
+)
 
 __all__ = [
+    "CAND001_STATE_CODEC_ID",
+    "CAND001_STATE_SCHEMA",
+    "Cand001PipelineStateCodec",
     "Cand001StrategyBinding",
     "Cand001StrategyPlugin",
     "adapt_cand001_runtime_candle",
